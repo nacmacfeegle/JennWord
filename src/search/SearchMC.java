b6360f83e43694c8784417945ff4ec7f47fd4d2f
nacmacfeegle <dan.costelloe@gmail.com>
2013-06-16 16:01:14 +0100

    reduced popsize, added userQuit flag, updated report

--- src/search/SearchMC.java
+++ src/search/SearchMC.java
@@ -6,39 +6,41 @@ import world.domain.Individual;
 
 /**
  * Flat Monte Carlo (i.e. strictly random) search.
+ * 
  * @author Cameron Browne
  */
-public class SearchMC extends Search 
-{
+public class SearchMC extends Search {
+	
+	private boolean userQuit = false;
 	/**
 	 * Constructor.
 	 */
-	public SearchMC()
-	{
+	public SearchMC() {
 		super("MC");
 	}
-	
-	//-------------------------------------------------------------------------
 
-	/** 
+	// -------------------------------------------------------------------------
+
+	/**
 	 * Perform a flat Monte Carlo (i.e. strictly random) search.
 	 */
 	@Override
-    public void search()
-    {
-		while (numTried < budget)  // each iteration involves one measure()
+	public void search() {
+		while (numTried < budget && !userQuit) // each iteration involves one measure()
 		{
-			//	Create a random individual
+			// Create a random individual
 			Individual indiv = domain.individual(domain.randomGenotype());
 			indiv.measure();
+			if (indiv.quality() == -1) {
+				userQuit = true;
+			}
 			numTried++;
-			
-			if (indiv.typicality() >= typicalityThreshold)
-			{
-				master.insert(indiv);  // add individual to master population
+
+			if (indiv.typicality() >= typicalityThreshold) {
+				master.insert(indiv); // add individual to master population
 				numTypical++;
 			}
 		}
-     }
-	
+	}
+
 }
--- src/world/World.java
+++ src/world/World.java
@@ -12,73 +12,65 @@ import search.SearchGA;
 import search.SearchMC;
 import world.domain.Domain;
 import world.domain.Individual;
-import world.domain.longlife.LongLifeDomain;
-//import world.domain.longlife.LongLifeDomain;
-//import world.domain.your.YourDomain;  // <-- import your domain here
+import world.domain.jenn.JennDomain;
 
 //-----------------------------------------------------------------------------
 
 /**
  * Main application for the "World in a Word" competition.
+ * 
  * @author Cameron Browne
  */
-public class World
-{	
+public class World {
 	/** Current domain. */
-	//protected Domain domain = new BiominoDomain();
-	protected Domain domain = new LongLifeDomain();
-	//protected Domain domain = new YourDomain();  // <-- construct your domain here
-	
+	protected Domain domain = new JennDomain();
+
 	/** Master population. */
 	protected Population population = new Population();
-	
+
 	/** List of search objects. */
 	private List<Search> searches = new ArrayList<Search>();
 
-	//-------------------------------------------------------------------------
-	
+	// -------------------------------------------------------------------------
+
 	/**
 	 * @return Currently active domain.
 	 */
-	public Domain domain()
-	{
+	public Domain domain() {
 		return domain;
 	}
-	
+
 	/**
 	 * @return Master population.
 	 */
-	public Population population()
-	{
+	public Population population() {
 		return population;
 	}
-	
-	//-------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------
 
 	/**
 	 * Perform non-static initialisation.
 	 */
-	private void init()
-	{
-		Utils.world = this;  // set before constructing Search objects
-		
-		searches.add(new SearchMC());
+	private void init() {
+		Utils.world = this; // set before constructing Search objects
+
+		// searches.add(new SearchMC());
 		searches.add(new SearchGA());
 	}
 
-	//-------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
 
 	/**
 	 * Run the experimental trials.
 	 */
-	void generate()
-	{
-		final int budget = 100000;  // number of individuals to measure
-		
+	void generate() {
+		// final int budget = 100000; // number of individuals to measure
+		final int budget = 10; // number of individuals to measure
+
 		System.out.println("Domain: " + domain.name());
 
-		for (int s = 0; s < searches.size(); s++)
-		{
+		for (int s = 0; s < searches.size(); s++) {
 			final Search search = searches.get(s);
 			population.clear();
 			search.conduct(budget);
@@ -86,78 +78,68 @@ public class World
 		exportExamples();
 	}
 
-	//-------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
 
 	/**
 	 * Export representative individuals to file.
 	 */
 	@SuppressWarnings("boxing")
-	void exportExamples()
-	{
+	void exportExamples() {
 		final int numToExport = 10;
-		
+
 		// Create "out" directory if needed
 		final String path = new String("out");
 		File dir = new File(path);
-		if (!dir.exists() && !dir.mkdir())    
-		{
-			System.out.println("Failed to create directory " + path + "."); 
+		if (!dir.exists() && !dir.mkdir()) {
+			System.out.println("Failed to create directory " + path + ".");
 			return;
 		}
- 		
+
 		// Empty the directory
-		final String[] files = dir.list();  
-		for (int i = 0; i < files.length; i++) 
-		{  
-			File file = new File(dir, files[i]);   
-			file.delete();  
-		} 		
+		final String[] files = dir.list();
+		for (int i = 0; i < files.length; i++) {
+			File file = new File(dir, files[i]);
+			file.delete();
+		}
 
 		// Create summary file and export
 		final String fileName = path + "/summary.txt";
-		try
-		{
+		try {
 			PrintStream out = new PrintStream(new FileOutputStream(fileName));
-						
-			//	Export representative individuals
-			for (int i = 0; i < numToExport && i < population.size(); i++)
-			{
+
+			// Export representative individuals
+			for (int i = 0; i < numToExport && i < population.size(); i++) {
 				final Individual indiv = population.get(i);
 				final String exportedTo = indiv.export(path);
-				if (exportedTo == "")
-				{
+				if (exportedTo == "") {
 					System.out.println("Failed to save file " + fileName + ".");
-				}
-				else
-				{
-					out.printf("%s: typicality=%.3f, quality=%.3f.\n", 
-						exportedTo, indiv.typicality(), indiv.quality());
+				} else {
+					out.printf("%s: typicality=%.3f, quality=%.3f.\n",
+							exportedTo, indiv.typicality(), indiv.quality());
 				}
 			}
 			out.close();
-		} 
-		catch (FileNotFoundException e) 
-		{
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-		
-	//-------------------------------------------------------------------------
-	
+
+	// -------------------------------------------------------------------------
+
 	/**
 	 * Main routine.
-	 * @param args program arguments. 
+	 * 
+	 * @param args
+	 *            program arguments.
 	 */
-	public static void main(String[] args) 
-	{	
+	public static void main(String[] args) {
 		System.out.println("WordWorld64 1.0\n");
-				
-		World world = new World();	
+
+		World world = new World();
 		world.init();
 		world.generate();
-		
+
 		System.out.println("\nDone.");
 	}
-	
-}
 
+}
